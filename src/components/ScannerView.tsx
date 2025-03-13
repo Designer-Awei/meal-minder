2a23ffc881b4aaf1a7b3184cfdde2d1936af65e8
Designer-Awei <1974379701@qq.com>
2025-03-13 14:53:13 +0800

    Update ScannerView.tsx
    调整下页面布局

--- src/components/ScannerView.tsx
+++ src/components/ScannerView.tsx
@@ -293,28 +293,28 @@ const ScannerView: React.FC = () => {
 
       {/* 扫描框 - 仅在未捕获图像时显示 */}
       {!capturedImage && (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[315px] h-[409px] z-10">
+        <div className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[340px] h-[420px] z-10">
           {/* 左上角 */}
           <motion.div 
-            className="absolute left-[10.95%] top-[19.45%] w-8 h-8 border-l-4 border-t-4 border-white"
+            className="absolute left-0 top-0 w-14 h-14 border-l-4 border-t-4 border-white"
             animate={{ opacity: [0.5, 1, 0.5] }}
             transition={{ duration: 2, repeat: Infinity }}
           />
           {/* 右上角 */}
           <motion.div 
-            className="absolute right-[10.7%] top-[19.45%] w-8 h-8 border-r-4 border-t-4 border-white"
+            className="absolute right-0 top-0 w-14 h-14 border-r-4 border-t-4 border-white"
             animate={{ opacity: [0.5, 1, 0.5] }}
             transition={{ duration: 2, repeat: Infinity }}
           />
           {/* 左下角 */}
           <motion.div 
-            className="absolute left-[10.95%] bottom-[33.74%] w-8 h-8 border-l-4 border-b-4 border-white"
+            className="absolute left-0 bottom-0 w-14 h-14 border-l-4 border-b-4 border-white"
             animate={{ opacity: [0.5, 1, 0.5] }}
             transition={{ duration: 2, repeat: Infinity }}
           />
           {/* 右下角 */}
           <motion.div 
-            className="absolute right-[10.7%] bottom-[33.74%] w-8 h-8 border-r-4 border-b-4 border-white"
+            className="absolute right-0 bottom-0 w-14 h-14 border-r-4 border-b-4 border-white"
             animate={{ opacity: [0.5, 1, 0.5] }}
             transition={{ duration: 2, repeat: Infinity }}
           />
@@ -323,13 +323,13 @@ const ScannerView: React.FC = () => {
 
       {/* 提示文本 - 仅在未捕获图像时显示 */}
       {!capturedImage && (
-        <div className="absolute left-1/2 transform -translate-x-1/2 top-[620px] px-4 py-2 bg-[rgba(56,48,46,0.13)] rounded-md z-10">
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-[170px] px-4 py-2 bg-[rgba(56,48,46,0.13)] rounded-md z-20">
           <p className="text-sm text-white text-center">支持小票/条形码/食材智能识别</p>
         </div>
       )}
 
       {/* 底部操作栏 */}
-      <div className="absolute bottom-0 left-0 right-0 h-[120px] bg-white rounded-t-3xl z-10">
+      <div className="absolute bottom-0 left-0 right-0 h-[150px] bg-white rounded-t-3xl z-10">
         <div className="flex flex-col items-center relative h-full">
           {/* 文字按钮区域 */}
           <div className="absolute top-0 left-0 right-0 flex justify-center items-center h-[50px] border-b border-gray-100">
@@ -369,7 +369,7 @@ const ScannerView: React.FC = () => {
           
           {/* 相册按钮 - 左下角 */}
           <button 
-            className="absolute left-[54px] bottom-[40px]"
+            className="absolute left-[54px] bottom-[50px]"
             onClick={selectFromGallery}
           >
             <div className="w-9 h-9 flex items-center justify-center">
@@ -381,7 +381,7 @@ const ScannerView: React.FC = () => {
           
           {/* 拍照按钮 - 居中 */}
           <motion.button 
-            className="absolute left-1/2 transform -translate-x-1/2 bottom-[30px]"
+            className="absolute left-1/2 transform -translate-x-1/2 bottom-[40px]"
             onClick={takePhoto}
             whileTap={{ scale: 0.95 }}
           >
@@ -393,7 +393,7 @@ const ScannerView: React.FC = () => {
           {/* 识别按钮 - 仅在已拍照时显示 */}
           {capturedImage && (
             <motion.button
-              className="absolute right-[54px] bottom-[40px] bg-orange-500 text-white rounded-full w-10 h-10 flex items-center justify-center"
+              className="absolute right-[54px] bottom-[50px] bg-orange-500 text-white rounded-full w-10 h-10 flex items-center justify-center"
               initial={{ scale: 0, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
               onClick={recognizeFood}
